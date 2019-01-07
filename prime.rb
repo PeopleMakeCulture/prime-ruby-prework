d79ce21d0b08554631fec69784a4b4fe43ed512e
Jing Cao <jingcao.yale@gmail.com>
2019-01-07 21:46:45 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,15 @@
 def prime?(int)
   return true
+
   while int > 1
     n = int - 1
+
     if int % n = 0
       return false
     end
-  else
-    n = n-1
+
+    else
+      n = n-1
+    end
   end
 end
